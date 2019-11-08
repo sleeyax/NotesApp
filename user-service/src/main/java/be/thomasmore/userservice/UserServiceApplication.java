0f8d72785cb45b9865f8ac0ac61544c39b0eba4b
karel roets <r0714682@student.thomasmore.be>
2019-11-08 09:13:55 +0100

    user-service mysql setup

--- user-service/src/main/java/be/thomasmore/userservice/UserServiceApplication.java
+++ user-service/src/main/java/be/thomasmore/userservice/UserServiceApplication.java
@@ -1,8 +1,12 @@
 package be.thomasmore.userservice;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient

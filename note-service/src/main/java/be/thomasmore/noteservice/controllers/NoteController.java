e70a5b2031a689aa282dbddb3212bcd24fe38d29
Sleeyax <yourd3veloper@gmail.com>
2019-11-14 23:06:15 +0100

    Add more HTTP methods

--- note-service/src/main/java/be/thomasmore/noteservice/controllers/NoteController.java
+++ note-service/src/main/java/be/thomasmore/noteservice/controllers/NoteController.java
@@ -4,7 +4,10 @@ import be.thomasmore.noteservice.entity.Note;
 import be.thomasmore.noteservice.repository.NoteRepository;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Date;
 import java.util.List;
@@ -22,19 +25,41 @@ public class NoteController {
 
     @PostMapping(value = "/")
     public Note postNote(@RequestBody Note note) {
+        if (note.getNote().equals(""))
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Note can't be empty!");
         note.setId(ObjectId.get().toString());
-        note.setCreatedAt(new Date());
-        repository.save(note);
-        return note;
+        note.setUpdatedAt(new Date());
+        return repository.save(note);
     }
 
     @GetMapping(value = "/search/userid={userid}")
-    public List<Note> getNotesByUserId(@PathVariable("userid") int userid) {
+    public List<Note> getNotesByUserId(@PathVariable("userid") long userid) {
         return repository.findAllByUserId(userid);
     }
 
     @GetMapping(value = "/{id}")
     public Note getNoteById(@PathVariable("id") String id) {
         return repository.findById(id).orElse(null);
+    }
+
+    @PutMapping(value = "/{id}")
+    public Note UpdateNote(@RequestBody Note updatedNote, @PathVariable String id) {
+        return repository.findById(id).map(note -> {
+            // update note
+            note.setUpdatedAt(new Date());
+            note.setNote(updatedNote.getNote());
+            return repository.save(note);
+        }).orElseGet(() -> {
+            // note not found in db, insert new one
+            updatedNote.setId(id);
+            updatedNote.setUpdatedAt(new Date());
+            return repository.save(updatedNote);
+        });
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteNote(@PathVariable String id) {
+        repository.deleteById(id);
+        return ResponseEntity.ok("");
     }
 }

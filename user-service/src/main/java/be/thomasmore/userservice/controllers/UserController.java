0bc3d8b6a5cbd826b7c3d4664b5b6fdfde3a4adc
karel roets <r0714682@student.thomasmore.be>
2019-11-15 08:54:09 +0100

    add, search mappings

--- user-service/src/main/java/be/thomasmore/userservice/controllers/UserController.java
+++ user-service/src/main/java/be/thomasmore/userservice/controllers/UserController.java
@@ -5,15 +5,15 @@ import be.thomasmore.userservice.repository.UserRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.netflix.discovery.converters.Auto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 @Controller
-@RequestMapping(path = "/demo")
+@RequestMapping(path = "/users")
 public class UserController
 {
-
     @Autowired
     private UserRepository userRepository;
 
@@ -31,6 +31,17 @@ public class UserController
     @GetMapping(path = "/all")
     public @ResponseBody Iterable<User> getAllUsers(){
         return userRepository.findAll();
+    }
+
+    @GetMapping(path = "/id")
+    public @ResponseBody User getUserById( @RequestParam int id) {
+        return userRepository.findUserById(id);
+    }
+
+    @DeleteMapping("delUser/{id}")
+    public @ResponseBody int delUserById(@PathVariable("id") int id){
+        Long deleted = userRepository.deleteById(id);
+        return deleted.intValue();
     }
 
 }
--- user-service/src/main/java/be/thomasmore/userservice/repository/UserRepository.java
+++ user-service/src/main/java/be/thomasmore/userservice/repository/UserRepository.java
@@ -8,4 +8,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @CrossOrigin
 public interface UserRepository extends JpaRepository<User, Integer> {
     User findUserByEmail(@Param("email") String email);
+    User findUserById(@Param("id") int id);
+    Long deleteById(int id);
 }

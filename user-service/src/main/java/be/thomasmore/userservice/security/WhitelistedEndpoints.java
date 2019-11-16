22563f5e9b1bf3e33d5fdb94d5080d6f3fd3a003
Sleeyax <yourd3veloper@gmail.com>
2019-11-17 00:04:45 +0100

    Delete class

--- user-service/src/main/java/be/thomasmore/userservice/security/WhitelistedEndpoints.java
+++
@@ -1,8 +0,0 @@
-package be.thomasmore.userservice.security;
-
-/**
- * List of URL endpoints that don't require authentication/authorization (JWT) in order to be accessed.
- */
-public class WhitelistedEndpoints {
-    public static final String ADD_USER = "/users/add";
-}

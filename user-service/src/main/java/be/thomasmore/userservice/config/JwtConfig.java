c679c24819222de13df388a04f7e4b88653d7af1
Sleeyax <yourd3veloper@gmail.com>
2019-11-16 23:54:31 +0100

    Change secret & login endpoint

--- user-service/src/main/java/be/thomasmore/userservice/config/JwtConfig.java
+++ user-service/src/main/java/be/thomasmore/userservice/config/JwtConfig.java
@@ -3,7 +3,7 @@ package be.thomasmore.userservice.config;
 import org.springframework.beans.factory.annotation.Value;
 
 public class JwtConfig {
-    @Value("${security.jwt.uri:/auth/**}")
+    @Value("${security.jwt.uri:/login}")
     private String Uri;
 
     @Value("${security.jwt.header:Authorization}")
@@ -15,7 +15,7 @@ public class JwtConfig {
     @Value("${security.jwt.expiration:#{24*60*60}}")
     private int expiration;
 
-    @Value("${security.jwt.secret:JwtSecretKey}")
+    @Value("${security.jwt.secret:nwl0Tp6QLGHBr5q}")
     private String secret;
 
     public String getUri() {

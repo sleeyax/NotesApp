e7564ff90e0f78a7fcd4134b14c17d0a756473e6
Sleeyax <yourd3veloper@gmail.com>
2019-11-24 00:13:51 +0100

    Delete SpellCheckResponse class

--- spell-check-service/src/main/java/be/thomasmore/spellcheckservice/controllers/SpellCheckController.java
+++ spell-check-service/src/main/java/be/thomasmore/spellcheckservice/controllers/SpellCheckController.java
@@ -2,10 +2,8 @@ package be.thomasmore.spellcheckservice.controllers;
 
 import be.thomasmore.spellcheckservice.api.GrammarBotApi;
 import be.thomasmore.spellcheckservice.entity.SpellCheckRequest;
-import be.thomasmore.spellcheckservice.entity.SpellCheckResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
